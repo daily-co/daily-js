4798f01db25bdee51637070c5ead8f73d3493583
Kwindla Hultman Kramer <kwindla@pluot.co>
2019-04-23 18:04:54 -0700

    message passing

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,20 @@
 // todo: add minified prod target
 
+import resolve from 'rollup-plugin-node-resolve';
+import commonJS from 'rollup-plugin-commonjs';
 import pkg from './package.json';
 
 export default [
   {
     input: 'src/module.js',
     output: [{ file: pkg.module, format: 'es' }],
+    plugins: [
+      resolve({
+        preferBuiltins: false,
+      }),
+      commonJS({
+        include: 'node_modules/**',
+      }),
+    ],
   },
 ];
