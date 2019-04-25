35fb4d5f33efaf21e73036d646b6fa103eb13a61
Kwindla Hultman Kramer <kwindla@pluot.co>
2019-04-25 14:26:41 -0700

    demo v1

--- src/module.js
+++ src/module.js
@@ -1,8 +1,40 @@
 import EventEmitter from 'events';
+import { deepEqual } from 'fast-equals';
 
 import {
-  IFRAME_MESSAGE_MARKER
+  // re-export
+  //
+  DAILY_STATE_NEW,
+  DAILY_STATE_JOINING,
+  DAILY_STATE_JOINED,
+  DAILY_STATE_LEFT,
+  DAILY_STATE_ERROR,
+
+  DAILY_EVENT_JOINING_MEETING,
+  DAILY_EVENT_JOINED_MEETING,
+  DAILY_EVENT_LEFT_MEETING,
+  DAILY_EVENT_PARTICIPANT_JOINED,
+  DAILY_EVENT_PARTICIPANT_UPDATED,
+  DAILY_EVENT_PARTICIPANT_LEFT,
+  
+  // internals
+  //
+  IFRAME_MESSAGE_MARKER,
+  DAILY_METHOD_LEAVE,
+  DAILY_METHOD_UPDATE_PARTICIPANT,
+  DAILY_METHOD_LOCAL_AUDIO,
+  DAILY_METHOD_LOCAL_VIDEO,
 } from './CommonIncludes.js';
+
+
+export { DAILY_STATE_NEW, DAILY_STATE_JOINING, DAILY_STATE_JOINED,
+         DAILY_STATE_LEFT, DAILY_STATE_ERROR,
+         DAILY_EVENT_JOINING_MEETING, DAILY_EVENT_JOINED_MEETING,
+         DAILY_EVENT_LEFT_MEETING };
+
+//
+//
+//
 
 const FRAME_PROPS = {
   url: {
@@ -15,30 +47,26 @@ const FRAME_PROPS = {
   }
 };
 
-// events:
-//   joining-meeting
-//   joined-meeting
-//   left-meeting
-//   error
-//   participant-joined
-//   participant-left
-//   participant-state-update
+// todo: more validation?
+const PARTICIPANT_PROPS = {
+  style: true, setAudio: true, setVideo: true, eject: true
+};
 
-// meeting states:
-//   '' (newly created)
-//   'joining'
-//   'joined'
-//   'left'
-//   'error'
+//
+//
+//
 
 export default class DailyIframe extends EventEmitter {
   constructor(iframeish, properties={}) {
     super();
     this.validateProperties(properties);
     this.properties = { ...properties };
-    this.iframe = iframeish;
-    this.meetingState = '';
-    this.messageCallbacks = {};
+
+    this._iframe = iframeish;
+    this._meetingState = DAILY_STATE_NEW;
+    this._participants = {};
+
+    this._messageCallbacks = {};
 
     window.addEventListener('message', (evt) => {
       if (evt.data && evt.data.what === 'iframe-call-message') {
@@ -48,14 +76,61 @@ export default class DailyIframe extends EventEmitter {
   }
 
   iframe() {
-    return this.iframe;
+    return this._iframe;
   }
 
   meetingState() {
-    return this.meetingState;
+    return this._meetingState;
   }
 
-  join(properties) {
+  participants() {
+    return this._participants;
+  }
+
+  // properties
+  //   style ?
+  //   setAudio
+  //   setVideo
+  //   eject
+  updateParticipant(id, properties) {
+    if (id && properties && this._participants[id]) {
+      for (var prop in properties) {
+        if (!PARTICIPANT_PROPS[prop]) {
+          throw new Error
+            (`unrecognized updateParticipant property ${prop}`);
+        }
+      }
+      this.sendMessage({ action: DAILY_METHOD_UPDATE_PARTICIPANT,
+                         id, properties});
+    }    
+    return this;
+  }
+
+  localAudio() {
+    if (this._participants.local) {
+      return this._participants.local.audio;
+    }
+    return null;
+  }
+
+  localVideo() {
+    if (this._participants.local) {
+      return this._participants.local.video;
+    }
+    return null;
+  }
+
+  setLocalAudio(bool) {
+    this.sendMessage({ action: DAILY_METHOD_LOCAL_AUDIO, state: bool });
+    return this;
+  }
+
+  setLocalVideo(bool) {
+    this.sendMessage({ action: DAILY_METHOD_LOCAL_VIDEO, state: bool });
+    return this;
+  }
+
+  async join(properties) {
     if (properties) {
       this.validateProperties(properties);
       this.properties = { ...this.properties, ...properties };
@@ -63,28 +138,29 @@ export default class DailyIframe extends EventEmitter {
     if (!this.properties.url) {
       throw new Error("can't join meeting because url property isn't set");
     }
-    this.meetingState = 'joining';
-    this.emit('joining');
-    this.iframe.src = this.properties.url;
+    this._meetingState = DAILY_STATE_JOINING;
+    this.emit(DAILY_EVENT_JOINING_MEETING,
+              { action: DAILY_EVENT_JOINING_MEETING });
+    this._iframe.src = this.properties.url;
+    return new Promise((resolve, reject) => {
+      this._joinedCallback = (participants) => {
+        if (participants) {
+          this._participants = participants;
+        }
+        resolve(participants);
+      }
+    })
   }
 
-  leave() {
+  async leave() {
     return new Promise((resolve, reject) => {
       let k = () => {
-        this.iframe.src = '';
-        this.emit('leave');
+        this._iframe.src = '';
+        this._meetingState = DAILY_STATE_LEFT;
+        this.emit(DAILY_STATE_LEFT, { action: DAILY_STATE_LEFT });
         resolve();
       }
-      this.sendMessage({ action: 'leave-mtg' }, k);
-    });
-  }
-
-  participants() {
-    return new Promise((resolve, reject) => {
-      let k = (msg) => {
-        resolve(msg.participants);
-      }
-      this.sendMessage({ action: 'participants' }, k);
+      this.sendMessage({ action: DAILY_METHOD_LEAVE }, k);
     });
   }
 
@@ -108,15 +184,50 @@ export default class DailyIframe extends EventEmitter {
     msg.what = IFRAME_MESSAGE_MARKER;
     if (callback) {
       let ts = Date.now();
-      this.messageCallbacks[ts] = callback;
+      this._messageCallbacks[ts] = callback;
       msg.callbackStamp = ts;
     }
-    this.iframe.contentWindow.postMessage(msg, '*');
+    this._iframe.contentWindow.postMessage(msg, '*');
   }
 
   handleMessage(msg) {
-    if (msg.callbackStamp && this.messageCallbacks[msg.callbackStamp]) {
-      this.messageCallbacks[msg.callbackStamp].call(this, msg);
+    // messages could be completely handled by callbacks
+    if (msg.callbackStamp && this._messageCallbacks[msg.callbackStamp]) {
+      this._messageCallbacks[msg.callbackStamp].call(this, msg);
+      delete this._messageCallbacks[msg.callbackStamp];
+    }
+    // or perhaps we should handle this message based on its
+    // msg.action tag. first we'll delete internal fields so the
+    // 'case' code blocks have the option of just emitting the raw
+    // message as an event
+    delete msg.what;
+    delete msg.callbackStamp;
+    switch (msg.action) {
+      case DAILY_EVENT_JOINED_MEETING:
+        if (this._joinedCallback) {
+          this._joinedCallback(msg.participants);
+          this._joinedCallback = null;
+        }
+        this.emit(msg.action, msg);
+        this._meetingState = DAILY_STATE_JOINED;
+        break;
+      case DAILY_EVENT_PARTICIPANT_JOINED:
+      case DAILY_EVENT_PARTICIPANT_UPDATED:
+        if (msg.participant && msg.participant.id) {
+          let id = msg.participant.local ? 'local' : msg.participant.id;
+          if (!deepEqual(msg.participant, this._participants[id])) {
+            this._participants[id] = msg.participant;
+            this.emit(msg.action, msg);
+          }
+        }
+        break;
+      case DAILY_EVENT_PARTICIPANT_LEFT:
+        if (msg.participant && msg.participant.id) {
+          delete this._participants[msg.participant.id];
+          this.emit(msg.action, msg);
+        }
+        break;
+      default: // no op
     }
   }
 
@@ -127,7 +238,8 @@ export default class DailyIframe extends EventEmitter {
   }
 
   static wrap(iframeish, properties={}) {
-    if (!iframeish || ('string' !== typeof iframeish.src)) {
+    if (!iframeish || !iframeish.contentWindow ||
+        ('string' !== typeof iframeish.src)) {
       throw new Error('DailyIframe::Wrap needs an iframe-like first argument');
     }
     return new DailyIframe(iframeish, properties);
