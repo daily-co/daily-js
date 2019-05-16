a0db656c0326ab789757ba03aeb15a8938f5aac6
Kwindla Hultman Kramer <kwindla@pluot.co>
2019-05-16 10:28:41 -0700

    more iframe lifecycle control

--- src/CommonIncludes.js
+++ src/CommonIncludes.js
@@ -4,12 +4,17 @@
 //
 
 export const DAILY_STATE_NEW = 'new';
+export const DAILY_STATE_LOADING = 'loading';
+export const DAILY_STATE_LOADED = 'loaded';
 export const DAILY_STATE_JOINING = 'joining-meeting';
 export const DAILY_STATE_JOINED = 'joined-meeting';
 export const DAILY_STATE_LEFT = 'left-meeting';
 export const DAILY_STATE_ERROR = 'error';
 
-
+export const DAILY_EVENT_LOADING = 'loading';
+export const DAILY_EVENT_LOADED = 'loaded';
+export const DAILY_EVENT_STARTED_CAMERA = 'started-camera';
+export const DAILY_EVENT_CAMERA_ERROR = 'camera-error';
 export const DAILY_EVENT_JOINING_MEETING = 'joining-meeting';
 export const DAILY_EVENT_JOINED_MEETING = 'joined-meeting';
 export const DAILY_EVENT_LEFT_MEETING = 'left-meeting';
@@ -34,6 +39,11 @@ export const DAILY_EVENT_ERROR = 'error';
 
 export const IFRAME_MESSAGE_MARKER = 'iframe-call-message';
 
+export const DAILY_METHOD_START_CAMERA = 'start-camera';
+export const DAILY_METHOD_SET_INPUT_DEVICES = 'set-input-devices';
+export const DAILY_METHOD_SET_OUTPUT_DEVICE = 'set-output-device';
+export const DAILY_METHOD_GET_INPUT_DEVICES = 'get-input-devices';
+export const DAILY_METHOD_JOIN = 'join-meeting';
 export const DAILY_METHOD_LEAVE = 'leave-meeting';
 export const DAILY_METHOD_UPDATE_PARTICIPANT = 'update-participant';
 export const DAILY_METHOD_LOCAL_AUDIO = 'local-audio';
@@ -45,3 +55,6 @@ export const DAILY_METHOD_STOP_RECORDING = 'local-recording-stop';
 export const DAILY_METHOD_LOAD_CSS = 'load-css';
 export const DAILY_METHOD_SET_BANDWIDTH = 'set-bandwidth';
 export const DAILY_METHOD_GET_CALC_STATS = 'get-calc-stats';
+export const DAILY_METHOD_ENUMERATE_DEVICES = 'enumerate-devices';
+export const DAILY_METHOD_CYCLE_CAMERA = 'cycle-camera';
+export const DAILY_METHOD_CYCLE_MIC = 'cycle-mic';
