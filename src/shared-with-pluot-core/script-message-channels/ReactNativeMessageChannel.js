baf6dda6e48e7bca13fda55cc627c5cfc91b95ac
Paul Kompfner <kompfner@gmail.com>
2020-04-30 17:16:10 -0400

    Handle callbacks in ReactNativeMessageChannel (though unfortunately this couldn't really be tested since loading the call machine hasn't been wired up yet)

--- src/shared-with-pluot-core/script-message-channels/ReactNativeMessageChannel.js
+++ src/shared-with-pluot-core/script-message-channels/ReactNativeMessageChannel.js
@@ -12,6 +12,7 @@ export default class ReactNativeMessageChannel extends ScriptMessageChannel {
   constructor() {
     super();
     this._wrappedListeners = {}; // Mapping between listeners and wrapped listeners
+    this._messageCallbacks = {};
   }
 
   addListenerForMessagesFromCallMachine(listener, callFrameId, thisValue) {
@@ -32,15 +33,16 @@ export default class ReactNativeMessageChannel extends ScriptMessageChannel {
     );
   }
 
-  sendMessageToCallMachine(message, callback, iframe, callFrameId) {
+  sendMessageToCallMachine(message, callback) {
     this._sendMessage(
       message,
       global.dailyJsToCallMachineEmitter,
-      'sending message to call machine'
+      'sending message to call machine',
+      callback
     );
   }
 
-  sendMessageToDailyJs(message, isCallObject, callFrameId) {
+  sendMessageToDailyJs(message) {
     this._sendMessage(
       message,
       global.callMachineToDailyJsEmitter,
@@ -60,15 +62,25 @@ export default class ReactNativeMessageChannel extends ScriptMessageChannel {
     const wrappedListener = (evt) => {
       const msg = evt.data;
       // console.log(`[ReactNativeMessageChannel] ${logMessage}`, msg);
+      if (msg.callbackStamp && this._messageCallbacks[msg.callbackStamp]) {
+        // console.log('[ReactNativeMessageChannel] handling message as callback', msg);
+        const callbackStamp = msg.callbackStamp; // Storing here since the callback could delete msg.callbackStamp
+        this._messageCallbacks[callbackStamp].call(thisValue, msg);
+        delete this._messageCallbacks[callbackStamp];
+      }
+      delete msg.callbackStamp;
       listener.call(thisValue, msg);
-      // TODO: handle callbacks and test
     };
     this._wrappedListeners[listener] = wrappedListener;
     messageEmitter.addListener('message', wrappedListener);
   }
 
-  _sendMessage(message, messageEmitter, logMessage) {
-    // TODO: handle adding callbackStamp
+  _sendMessage(message, messageEmitter, logMessage, callback) {
+    if (callback) {
+      let ts = Date.now();
+      this._messageCallbacks[ts] = callback;
+      message.callbackStamp = ts;
+    }
     // console.log(`[ReactNativeMessageChannel] ${logMessage}`, message);
     messageEmitter.emit('message', message);
   }
