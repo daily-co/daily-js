a7cc9e403a1d0b090399067e711ddf8843dfcb2c
Paul Kompfner <kompfner@gmail.com>
2020-05-06 15:58:05 -0400

    Update to the latest call object bundle URL construction logic, which was undone during a rebase.

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,5 @@
+import { browserInfo } from './shared-with-pluot-core/Environment';
+
 export function notImplementedError() {
   throw new Error('Method must be implemented in subclass');
 }
@@ -5,9 +7,17 @@ export function notImplementedError() {
 export function callObjectBundleUrl(meetingUrl) {
   // Use the CDN to get call-machine-object (but use whatever's "local" for dev+staging)
   if (process.env.NODE_ENV === 'production') {
-    return `https://c.daily.co/static/call-machine-object-bundle.js`;
+    if (!browserInfo().supportsSfu) {
+      return `https://c.daily.co/static/call-machine-object-nosfu-bundle.js`;
+    } else {
+      return `https://c.daily.co/static/call-machine-object-bundle.js`;
+    }
   } else {
     let url = new URL(meetingUrl);
-    return `${url.origin}/static/call-machine-object-bundle.js`;
+    if (!browserInfo().supportsSfu) {
+      return `${url.origin}/static/call-machine-object-nosfu-bundle.js`;
+    } else {
+      return `${url.origin}/static/call-machine-object-bundle.js`;
+    }
   }
 }
