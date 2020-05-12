20cf4380b203982bca573b6769ec00377bba7a1a
Paul Kompfner <kompfner@gmail.com>
2020-05-12 15:51:26 -0400

    Avoid trying to look up browser information (which will fail) when running in React Native. This lookup recently became part of the normal `join()` code path as part of this change: https://github.com/daily-co/daily-js/commit/6269ee6be6943c96b0eed6b7d8d0e13dfb064ddf

--- src/shared-with-pluot-core/Environment.js
+++ src/shared-with-pluot-core/Environment.js
@@ -5,6 +5,17 @@ export function isReactNative() {
 }
 
 export function browserInfo() {
+  if (isReactNative()) {
+    return {
+      supported: true,
+      mobile: true,
+      name: "React Native",
+      version: null,
+      supportsScreenShare: false,
+      supportsSfu: true,
+    };
+  }
+
   function supportsUnifiedPlanSDP(browser) {
     return browser.satisfies({
       electron: ">=6",
