99b40b53bb2d43d8605a6681fa9e656bd36f797e
kompfner <kompfner@gmail.com>
2022-09-22 09:59:06 -0400

    Wire up server -> client syncing of meeting session data

--- src/shared-with-pluot-core/CommonIncludes.js
+++ src/shared-with-pluot-core/CommonIncludes.js
@@ -79,6 +79,8 @@ export const DAILY_EVENT_PARTICIPANT_COUNTS_UPDATED =
 export const DAILY_EVENT_ACCESS_STATE_UPDATED = 'access-state-updated';
 
 export const DAILY_EVENT_MEETING_SESSION_UPDATED = 'meeting-session-updated';
+export const DAILY_EVENT_MEETING_SESSION_STATE_UPDATED =
+  'meeting-session-state-updated';
 
 export const DAILY_EVENT_WAITING_PARTICIPANT_ADDED =
   'waiting-participant-added';
