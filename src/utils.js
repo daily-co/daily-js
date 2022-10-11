ea9f97a6e3e6c683694db256922607fa3c62d583
kompfner <kompfner@gmail.com>
2022-10-10 20:27:00 -0400

    Add comment explaining daily-js's choice of call machine bundle to download

--- src/utils.js
+++ src/utils.js
@@ -12,6 +12,12 @@ export function callObjectBundleUrl() {
     return window._dailyConfig.callObjectBundleUrlOverride;
   }
 
+  // 1. Dev build of daily-js --> load bundle from __devCallMachineUrl__, which
+  //    is either:
+  //    - DEV_CALL_MACHINE_URL env variable (read at build time)
+  //    - default local dev URL
+  //    See webpack or rollup config for details.
+  // 2. Prod build of daily-js --> load bundle from version-specific prod URL.
   return process.env.NODE_ENV === 'development'
     ? __devCallMachineUrl__
     : `https://c.daily.co/call-machine/versioned/${__dailyJsVersion__}/static/call-machine-object-bundle.js`;
