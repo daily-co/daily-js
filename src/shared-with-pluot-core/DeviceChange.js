3aa77c39e74f842d46b9c1cf8e06bb14c857ed64
kompfner <kompfner@gmail.com>
2023-04-07 11:12:45 -0400

    fix(dj): Fix broken prebuilt build due to server-side running of code only meant for client-side execution

--- src/shared-with-pluot-core/DeviceChange.js
+++ src/shared-with-pluot-core/DeviceChange.js
@@ -2,11 +2,23 @@ import { isReactNative } from './Environment';
 import * as EventBased from './DeviceChange_EventBased';
 import * as PollBased from './DeviceChange_PollBased';
 
+export function addDeviceChangeListener(deviceChangeListener) {
+  _supportsEventBasedDeviceChangeDetection()
+    ? EventBased.addDeviceChangeListener(deviceChangeListener)
+    : PollBased.addDeviceChangeListener(deviceChangeListener);
+}
+
+export function removeDeviceChangeListener(deviceChangeListener) {
+  _supportsEventBasedDeviceChangeDetection()
+    ? EventBased.removeDeviceChangeListener(deviceChangeListener)
+    : PollBased.removeDeviceChangeListener(deviceChangeListener);
+}
+
 // Desktop web, iOS web, and React Native support the 'devicechange' event.
 // Android Chrome/Samsung Internet doesn't.
-const supportsEventBasedDeviceChangeDetection =
-  isReactNative() ||
-  typeof navigator.mediaDevices.ondevicechange !== 'undefined';
-const { addDeviceChangeListener, removeDeviceChangeListener } =
-  supportsEventBasedDeviceChangeDetection ? EventBased : PollBased;
-export { addDeviceChangeListener, removeDeviceChangeListener };
+function _supportsEventBasedDeviceChangeDetection() {
+  return (
+    isReactNative() ||
+    typeof navigator.mediaDevices.ondevicechange !== 'undefined'
+  );
+}
