6f89cf97c3b50cf372fa06e2c083dfb8655cc75f
mattie ruth backman <mattieruth@gmail.com>
2023-08-29 15:57:13 -0400

    fix(dj): ignore transient mute states on remote chrome screenshares

--- src/shared-with-pluot-core/TrackStateUtil.js
+++ src/shared-with-pluot-core/TrackStateUtil.js
@@ -8,11 +8,15 @@ function isTrackMuted(track, { isLocalScreenVideo }) {
   // messes with our ability to determine whether the track is *actually* muted,
   // so here we simply treat them as not `muted` (the overwhelmingly likely
   // scenario).
+  // Note: for remote screenshare tracks we rely on the _maybeTransientMute
+  // flag below. We can't do this for the local screenshare because we
+  // don't have a mute handler attached but also, they don't seem to be so
+  // transient
   if (isLocalScreenVideo && getBrowserName() === 'Chrome') {
     return false;
   }
 
-  return track.muted;
+  return track.muted && !track._maybeTransientMute;
 }
 
 export function isPlayable(track, { isLocalScreenVideo }) {
