f3e4f05cb7c52f69f33f3da8a2ae9de445284cab
mattie ruth backman <mattieruth@gmail.com>
2023-10-23 14:22:15 -0400

    move average to overall call stats and rename vars

--- index.d.ts
+++ index.d.ts
@@ -683,8 +683,7 @@ export interface DailyNetworkStats {
       timestamp: number;
       recvBitsPerSecond: number | null;
       sendBitsPerSecond: number | null;
-      curNetworkRTT: number | null;
-      aveNetworkRTT: number | null;
+      networkRoundTripTime: number | null;
       videoRecvBitsPerSecond: number | null;
       videoSendBitsPerSecond: number | null;
       audioRecvBitsPerSecond: number | null;
@@ -708,6 +707,7 @@ export interface DailyNetworkStats {
     worstVideoSendJitter: number;
     worstAudioRecvJitter: number;
     worstAudioSendJitter: number;
+    averageNetworkRoundTripTime: number;
   };
   threshold: 'good' | 'low' | 'very-low';
 }
