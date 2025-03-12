4c31b9f0d327580a4b65398a99400aaec692fb4f
mattie ruth backman <mattieruth@gmail.com>
2025-03-12 17:18:17 -0400

    typos

--- index.d.ts
+++ index.d.ts
@@ -606,7 +606,7 @@ export interface DailyParticipant {
   joined_at?: Date;
   networkQualityState?: 'good' | 'warning' | 'bad' | 'unknown';
   /**
-   * @deprecated This property will is being replaced by networkState.
+   * @deprecated This property is being replaced by networkState.
    */
   networkThreshold?: 'good' | 'low' | 'very-low';
   will_eject_at: Date;
@@ -904,11 +904,11 @@ export interface DailyNetworkStats {
   networkStateReasons: networkStateReasons[];
   stats: Record<string, never> | DailyNetworkStatsData;
   /**
-   * @deprecated This property will is being replaced by networkState.
+   * @deprecated This property is being replaced by networkState.
    */
   threshold: 'good' | 'low' | 'very-low';
   /**
-   * @deprecated This property will is being replaced by networkState.
+   * @deprecated This property is being replaced by networkState.
    */
   quality: number;
 }
